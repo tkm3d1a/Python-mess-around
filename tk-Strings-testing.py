3a7012d5ec41dee5bbb0a078f4868f369630d833
tkm3d1a <tim.klimpel@gmail.com>
2021-01-04 22:10:39 -0500

    commit 1-4-20 - strings messing around 2

--- tk-Strings-testing.py
+++ tk-Strings-testing.py
@@ -8,9 +8,18 @@ count = 0
 
 if count <= phrase_len:
     print(phrase[count])
-    count = count + 1  #bad loop
+    count = count + 1  # bad loop
 
 count = 0
 
-for count in phrase:
+for count in phrase:  # will print the letters one by one, new line for each print
     print(count)
+
+
+# below is a bad implementation, doesnt even do anything.  don't know syntax for shit...
+# count = 0
+# newPhrase = ""
+#
+# for count in phrase_len:
+#     newPhrase = newPhrase + phrase[count]
+#     print(newPhrase)
